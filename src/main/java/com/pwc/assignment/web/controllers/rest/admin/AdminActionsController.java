da5df206abe0fd2ddaef2d3fd058c6627c45af9f
OsamaAbuHamdan <ohamdan@atypon.com>
2021-05-22 00:22:31 +0300

    Enhancing AdminActionsController to support some filtering

--- src/main/java/com/pwc/assignment/web/controllers/rest/admin/AdminActionsController.java
+++ src/main/java/com/pwc/assignment/web/controllers/rest/admin/AdminActionsController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController()
+@RestController
 @RequestMapping("/admin")
 public class AdminActionsController {
     @Autowired
@@ -25,18 +25,20 @@ public class AdminActionsController {
 
     @PostMapping(path = "/changeUserRole")
     public ResponseEntity<HttpStatus> changeUserRole(@RequestParam String userName, @RequestParam String newRole) {
-        userService.changeUserAuthority(userName, UserRoles.ofString(newRole));
+        userService.changeUserAuthority(userName, UserRoles.valueOf(newRole));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping(path = "/changeComplaintStatus")
     public ResponseEntity<HttpStatus> changeComplaintStatus(@RequestParam String complaintId, @RequestParam String newStatus) {
-        complaintService.changeComplaintStatusById(complaintId, ComplaintStatus.ofString(newStatus));
+        complaintService.changeComplaintStatusById(complaintId, ComplaintStatus.valueOf(newStatus));
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
-    @GetMapping(path = "/listAllUserComplaints")
-    public List<Complaint> listUserComplaints(@RequestParam String username) {
+    @GetMapping(path = "/listComplaints")
+    public List<Complaint> listComplaints(@RequestParam(required = false) String username) {
+        if (username == null)
+            return complaintService.getAllComplaints();
         return complaintService.getAllUserComplaints(username);
     }
 
