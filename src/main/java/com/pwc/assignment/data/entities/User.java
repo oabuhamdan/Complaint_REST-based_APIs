dad55a2e60ee242185c4ff496649ae870533a504
OsamaAbuHamdan <ohamdan@atypon.com>
2021-05-22 20:08:38 +0300

    Remove UUID for user and make username the id

--- src/main/java/com/pwc/assignment/data/entities/User.java
+++ src/main/java/com/pwc/assignment/data/entities/User.java
@@ -1,8 +1,8 @@
 package com.pwc.assignment.data.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pwc.assignment.data.records.UserRecord;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -13,15 +13,14 @@ import java.util.List;
 @NoArgsConstructor
 @RequiredArgsConstructor
 @EqualsAndHashCode(exclude = "complaints")
-public class User implements UserRecord  {
-    @Id @GeneratedValue(generator="system-uuid")
-    @GenericGenerator(name="system-uuid", strategy = "uuid")
-    private String id;
-    @NonNull
-    private String name;
+public class User implements UserRecord {
+    @Id
     @NonNull
     private String username;
     @NonNull
+    private String name;
+    @NonNull
+    @JsonIgnore
     private String password;
     @NonNull
     private UserRoles authority;
